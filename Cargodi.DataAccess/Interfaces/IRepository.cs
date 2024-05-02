7327b98cf072ac2ba538cf9edaa0920235dfdaa3
VlazhV <danilabondarev1234@gmail.com>
2024-05-02 20:36:58 +0300

    repo base added

---
+++ Cargodi.DataAccess/Interfaces/IRepository.cs
@@ -0,0 +1,11 @@
+namespace Cargodi.DataAccess.Interfaces;
+
+public interface IRepository<T, K> where T: class
+{
+	Task<T?> GetByIdAsync(K id, CancellationToken cancellationToken);
+	Task<IEnumerable<T>> GetAsync(CancellationToken cancellationToken);
+	Task<T> CreateAsync(T entity, CancellationToken cancellationToken);
+	T Update(T entity);
+	T Delete(T entity);
+	Task SaveChangesAsync(CancellationToken cancellationToken);
+}
---
+++ Cargodi.DataAccess/Interfaces/RepositoryBase.cs
@@ -0,0 +1,57 @@
+
+using Cargodi.DataAccess.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cargodi.DataAccess.Interfaces;
+
+public class RepositoryBase<T, K> : IRepository<T, K> where T: class
+{
+protected readonly DatabaseContext _db;
+	
+	public RepositoryBase(DatabaseContext db)
+	{
+		_db = db;
+	}
+	
+	public async Task<T> CreateAsync(T entity, CancellationToken cancellationToken)
+	{
+		var entry = await _db.AddAsync(entity!, cancellationToken);
+
+		return entry.Entity;
+	}
+
+	public T Delete(T entity)
+	{
+		var entry = _db.Remove(entity!);
+
+		return entry.Entity;
+	}
+
+	public async Task<IEnumerable<T>> GetAllAsync(CancellationToken cancellationToken)
+	{
+		return await _db.Set<T>().AsNoTracking().ToListAsync(cancellationToken);
+	}
+
+    public Task<IEnumerable<T>> GetAsync(CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<T?> GetByIdAsync(K id, CancellationToken cancellationToken)
+	{
+		return await _db.Set<T>().FindAsync(id, cancellationToken);
+	}
+
+	public async Task SaveChangesAsync(CancellationToken cancellationToken)
+	{
+		await _db.SaveChangesAsync(cancellationToken);
+	}
+
+	public T Update(T entity)
+	{
+		var entry = _db.Update(entity);
+
+		return entry.Entity;
+	}
+
+}
