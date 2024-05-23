fa1a84e1634181aeba7eaf9dea446e8b72ce758b
VlazhV <danilabondarev1234@gmail.com>
2024-05-23 20:18:37 +0300

    cars controller [authorize[

--- Cargodi.WebApi/Controllers/Autopark/CarsController.cs
+++ Cargodi.WebApi/Controllers/Autopark/CarsController.cs
@@ -2,12 +2,14 @@ using Cargodi.Business.DTOs.Autopark.Car;
 using Cargodi.Business.Interfaces.Autopark;
 using Cargodi.DataAccess.Constants;
 using Cargodi.WebApi.Extensions;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Cargodi.WebApi.Controllers.Autopark;
 
 [Route("api/cars")]
 [ApiController]
+[Authorize]
 public class CarsController: ControllerBase
 {
     private readonly ICarService _carService;
@@ -58,7 +60,7 @@ public class CarsController: ControllerBase
     public async Task<ActionResult<GetCarDto>> UpdateAsync([FromRoute] int id, UpdateCarDto carDto, CancellationToken cancellationToken)
     {
         if (!User.IsInRole(Roles.Admin) && !User.IsInRole(Roles.Manager))
-			return NotFound();
+            return NotFound();
         
         return Ok(await _carService.UpdateAsync(id, carDto, cancellationToken));	
     }
