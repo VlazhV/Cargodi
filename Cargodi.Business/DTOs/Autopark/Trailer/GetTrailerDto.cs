3fc3a329751c2c6ada4f97ff16dfd79764bb007b
VlazhV <danilabondarev1234@gmail.com>
2024-05-24 21:18:39 +0300

    cardto trailerdto updated

--- Cargodi.Business/DTOs/Autopark/Trailer/GetTrailerDto.cs
+++ Cargodi.Business/DTOs/Autopark/Trailer/GetTrailerDto.cs
@@ -3,11 +3,14 @@ namespace Cargodi.Business.DTOs.Autopark.Trailer;
 public class GetTrailerDto
 {
     public int Id { get; set; }
-	public string LicenseNumber { get; set; } = null!;
+    public string LicenseNumber { get; set; } = null!;
 
-	public int CapacityLength { get; set; } //mm
-	public int CapacityWidth { get; set; } //mm	
-	public int CapacityHeight { get; set; } //mm	
+    public int CapacityLength { get; set; } //mm
+    public int CapacityWidth { get; set; } //mm	
+    public int CapacityHeight { get; set; } //mm	
 
-	public int Carrying { get; set; }
+    public int Carrying { get; set; }
+    
+    public int AutoparkId { get; set; }
+    public int ActualAutoparkId { get; set; }
 }
