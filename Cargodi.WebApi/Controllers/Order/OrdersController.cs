1e74f43194546334649a3c0949dc02110f18b900
VlazhV <danilabondarev1234@gmail.com>
2024-05-25 19:04:45 +0300

    ship route

--- Cargodi.WebApi/Controllers/Order/OrdersController.cs
+++ Cargodi.WebApi/Controllers/Order/OrdersController.cs
@@ -1,3 +1,4 @@
+using System.Security.Claims;
 using Cargodi.Business.DTOs.Order.Order;
 using Cargodi.Business.DTOs.Order.Payload;
 using Cargodi.Business.Interfaces.Order;
@@ -13,64 +14,69 @@ namespace Cargodi.WebApi.Controllers.Order;
 [Authorize]
 public class OrdersController: ControllerBase
 {
-	private readonly IOrderService _orderService;
-	
-	public OrdersController(IOrderService orderService)
-	{
-		_orderService = orderService;
-	}
-	
-	[HttpGet] 
-	public async Task<ActionResult<IEnumerable<GetOrderInfoDto>>> GetAllAsync(CancellationToken cancellationToken)
-	{
-		if (!User.IsInRole(Roles.Admin) && !User.IsInRole(Roles.Manager))
-			return NotFound();
-			
-		return Ok(await _orderService.GetAllAsync(cancellationToken));
-	}
-	
-	[HttpGet("my")]
-	public async Task<ActionResult<IEnumerable<GetOrderInfoDto>>> GetAllOfClientAsync(long userId, CancellationToken cancellationToken)
-	{        
-		return Ok(await _orderService.GetAllOfClientAsync(userId, cancellationToken));
-	}
-	
-	[HttpGet("{id}")]
-	public async Task<ActionResult<GetOrderInfoDto>> GetByIdAsync(long id, CancellationToken cancellationToken)
-	{
-		return Ok(await _orderService.GetByIdAsync(id, User, cancellationToken));
-	}
-	
-	[HttpPost]
-	public async Task<ActionResult<GetOrderDto>> CreateAsync([FromQuery] long? customerId, [FromBody] UpdateOrderPayloadsDto orderDto, CancellationToken cancellationToken)
-	{		
-		return Ok(await _orderService.CreateAsync(customerId, User, orderDto, cancellationToken));
-	}
-	
-	[HttpPut("{id}")]
-	public async Task<ActionResult<GetOrderDto>> UpdateAsync(long id, [FromBody] UpdateOrderDto orderDto, CancellationToken cancellationToken)
-	{
-		return Ok(await _orderService.UpdateAsync(id, User, orderDto, cancellationToken));
-	}
-	
-	[HttpPatch("{id}")]
-	public async Task<ActionResult<GetOrderInfoDto>> UpdatePayloadListAsync
-		(long id, [FromBody] IEnumerable<UpdatePayloadDto> payloadDtos, CancellationToken cancellationToken)
-	{
-		return Ok(await _orderService.UpdatePayloadListAsync(id, User, payloadDtos, cancellationToken));
-	}
-	
-	[HttpDelete("{id}")]
-	public async Task<ActionResult> DeleteAsync(long id, CancellationToken cancellationToken)
-	{
-		await _orderService.DeleteAsync(id, User, cancellationToken);
+    private readonly IOrderService _orderService;
+    
+    public OrdersController(IOrderService orderService)
+    {
+        _orderService = orderService;
+    }
+    
+    [HttpGet] 
+    public async Task<ActionResult<IEnumerable<GetOrderInfoDto>>> GetAllAsync(CancellationToken cancellationToken)
+    {
+        if (!User.IsInRole(Roles.Admin) && !User.IsInRole(Roles.Manager))
+            return NotFound();
+            
+        return Ok(await _orderService.GetAllAsync(cancellationToken));
+    }
+    
+    [HttpGet("my")]
+    public async Task<ActionResult<IEnumerable<GetOrderInfoDto>>> GetAllOfClientAsync(CancellationToken cancellationToken)
+    {
+        if (!User.IsInRole(Roles.Client))
+            return NotFound();
+        
+        var userIdString = User.FindFirst(ClaimTypes.NameIdentifier)!.Value;
+        var userId = long.Parse(userIdString);
+        return Ok(await _orderService.GetAllOfClientAsync(userId, cancellationToken));
+    }
+    
+    [HttpGet("{id}")]
+    public async Task<ActionResult<GetOrderInfoDto>> GetByIdAsync(long id, CancellationToken cancellationToken)
+    {
+        return Ok(await _orderService.GetByIdAsync(id, User, cancellationToken));
+    }
+    
+    [HttpPost]
+    public async Task<ActionResult<GetOrderDto>> CreateAsync([FromQuery] long? customerId, [FromBody] UpdateOrderPayloadsDto orderDto, CancellationToken cancellationToken)
+    {		
+        return Ok(await _orderService.CreateAsync(customerId, User, orderDto, cancellationToken));
+    }
+    
+    [HttpPut("{id}")]
+    public async Task<ActionResult<GetOrderDto>> UpdateAsync(long id, [FromBody] UpdateOrderDto orderDto, CancellationToken cancellationToken)
+    {
+        return Ok(await _orderService.UpdateAsync(id, User, orderDto, cancellationToken));
+    }
+    
+    [HttpPatch("{id}")]
+    public async Task<ActionResult<GetOrderInfoDto>> UpdatePayloadListAsync
+        (long id, [FromBody] IEnumerable<UpdatePayloadDto> payloadDtos, CancellationToken cancellationToken)
+    {
+        return Ok(await _orderService.UpdatePayloadListAsync(id, User, payloadDtos, cancellationToken));
+    }
+    
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> DeleteAsync(long id, CancellationToken cancellationToken)
+    {
+        await _orderService.DeleteAsync(id, User, cancellationToken);
 
-		return NoContent();
-	}
-	
-	[HttpPut("{id}/status")]
-	public async Task<ActionResult<GetOrderDto>> SetStatusAsync(long id, [FromQuery] string status, CancellationToken cancellationToken)
-	{
-		return Ok(await _orderService.SetStatusAsync(id, status, User, cancellationToken));
-	}
+        return NoContent();
+    }
+    
+    [HttpPut("{id}/status")]
+    public async Task<ActionResult<GetOrderDto>> SetStatusAsync(long id, [FromQuery] string status, CancellationToken cancellationToken)
+    {
+        return Ok(await _orderService.SetStatusAsync(id, status, User, cancellationToken));
+    }
 }
